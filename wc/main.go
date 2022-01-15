efb46e14977a637810013d126f55a639e250305b
Rodrigo Machuca <rmachuca89@hotmail.com>
2022-01-15 10:42:58 -0600

    [wc] Adds count bytes feature with test

--- wc/main.go
+++ wc/main.go
@@ -9,30 +9,45 @@ import (
 )
 
 var (
-	// Defining a boolean flag `-l` to count lines instead of words
+	// Defining a boolean flag `-w` to count words
+	words = flag.Bool("w", true, "Count words")
+
+	// Defining a boolean flag `-l` to count lines
 	lines = flag.Bool("l", false, "Count lines")
+
+	// Defining a boolean flag `-c` to count bytes
+	btes = flag.Bool("c", false, "Count bytes")
 )
 
-func count(r io.Reader, countLines bool) int {
+func count(r io.Reader, countLines bool, countWords bool, countBytes bool) int {
 	// A scanner is used to read text from a Reader (such as files or STDIN)
 	scanner := bufio.NewScanner(r)
 
-	// If the count lines flag is not set, we want to count words, so we define
-	// the scanner split type to words (default is split by lines)
-	if !countLines {
+	// Set count type to scan. Mutually exclusive as they override each other,
+	// having bytes with the highest priority.
+	if countLines {
+		scanner.Split(bufio.ScanLines)
+	}
+
+	if countWords {
 		scanner.Split(bufio.ScanWords)
 	}
 
+	// Note that a string is a sequence of bytes, not runes.
+	if countBytes {
+		scanner.Split(bufio.ScanBytes)
+	}
+
 	//Defining an internal scoped word counter
-	wc := 0
+	counter := 0
 
 	// For every word scanned, increment the counter by one
 	for scanner.Scan() {
-		wc++
+		counter++
 	}
 
-	// Return the total word counter
-	return wc
+	// Return the total counter
+	return counter
 }
 
 func main() {
@@ -41,5 +56,5 @@ func main() {
 
 	// Calling the count function to count the number of words (or lines)
 	// received from the Standard Input and printing it out
-	fmt.Println(count(os.Stdin, *lines))
+	fmt.Println(count(os.Stdin, *lines, *words, *btes))
 }
--- wc/main_test.go
+++ wc/main_test.go
@@ -5,11 +5,16 @@ import (
 	"testing"
 )
 
+var (
+	testString = "word1 word2 word3\nline2\nline3 word1"
+)
+
 // TestCountWords tests the count function set to count words
 func TestCountWords(t *testing.T) {
-	b := bytes.NewBufferString("word1 wod2 word3 word4\n")
-	expected := 4
-	result := count(b, false)
+	b := bytes.NewBufferString(testString)
+
+	expected := 6
+	result := count(b, false, true, false)
 
 	if result != expected {
 		t.Errorf("Expected: %d, got %d instead\n", expected, result)
@@ -17,9 +22,21 @@ func TestCountWords(t *testing.T) {
 }
 
 func TestCountLines(t *testing.T) {
-	b := bytes.NewBufferString("word1 wod2 word3\nline2\nline3 word1")
+	b := bytes.NewBufferString(testString)
+
 	expected := 3
-	result := count(b, true)
+	result := count(b, true, false, false)
+
+	if result != expected {
+		t.Errorf("Expected: %d, got %d instead\n", expected, result)
+	}
+}
+
+func TestCountBytes(t *testing.T) {
+	b := bytes.NewBufferString(testString)
+
+	expected := 35
+	result := count(b, false, false, true)
 
 	if result != expected {
 		t.Errorf("Expected: %d, got %d instead\n", expected, result)
