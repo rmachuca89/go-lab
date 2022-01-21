1fc8873dadaf22cdc6449cb0f23b3a35f4e7b84b
Rodrigo Machuca <rmachuca89@hotmail.com>
2022-01-21 17:48:38 -0600

    [todo] Implements Tasks.Get method with tests

--- pkg/todo/todo.go
+++ pkg/todo/todo.go
@@ -35,5 +35,26 @@ func (t *Tasks) Save(title string) error {
 }
 
 func (t *Tasks) Get(title string) *Task {
-	return new(Task)
+	// Dereference the current pointer, as indexing is only supported on values.
+	tL := *t
+
+	g := new(Task)
+	taskIndex := indexOfTitle(tL, title)
+
+	if taskIndex > -1 {
+		g = &tL[taskIndex]
+	}
+
+	return g
+}
+
+// IndexOfTitle returns an index `int` value by searching current Tasks by Task.title.
+// Returns `-1` when the provided title was not found in the data set.
+func indexOfTitle(tL Tasks, title string) int {
+	for i, task := range tL {
+		if task.Title == title {
+			return i
+		}
+	}
+	return -1
 }
