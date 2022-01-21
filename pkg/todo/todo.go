8a6d1649bc78bdaea0282c63e6326579d03af8db
Rodrigo Machuca <rmachuca89@hotmail.com>
2022-01-20 18:21:33 -0600

    [wip] Implement TODO tests

--- pkg/todo/todo.go
+++ pkg/todo/todo.go
@@ -5,23 +5,17 @@ import (
 	"time"
 )
 
-type task struct {
-	uuid        string
-	title       string
-	description string
-	createdAt   time.Time
-	completed   bool
-	completedAt time.Time
+type Task struct {
+	Title       string
+	CreatedAt   time.Time
+	Completed   bool
+	CompletedAt time.Time
 }
 
-type Tasks []task
+type Tasks []Task
 
-func (t *Tasks) List() (*Tasks, error) {
-	return new(Tasks), errors.New("not yet implemented")
-}
-
-func (t *Tasks) Add(title, desc string) (*task, error) {
-	return new(task), errors.New("not yet implemented")
+func (t *Tasks) Add(title string) (*Task, error) {
+	return new(Task), errors.New("not yet implemented")
 }
 
 func (t *Tasks) Complete(title string) error {
@@ -36,6 +30,6 @@ func (t *Tasks) Save(title string) error {
 	return errors.New("not yet implemented")
 }
 
-func (t *Tasks) Get(title string) error {
-	return errors.New("not yet implemented")
+func (t *Tasks) Get(title string) *Task {
+	return new(Task)
 }
