477e591c0337f82f989c961372ed5bbc1ef656cf
Rodrigo Machuca <rmachuca89@hotmail.com>
2022-02-25 17:23:10 -0600

    [multifunc] Implements concurrent calls with goroutines and a WaitGroup

--- multifunc/main.go
+++ multifunc/main.go
@@ -1,39 +1,62 @@
 // Package multifunc illustrates how Go uses goroutines and channels to make
 // concurrency a first class citizen.
 //
-// In this first iteration, the code is executed secuentially and blocking;
-// if hypotetically each call took 3 seconds to run, it would take 6 seconds
-// total for main to complete its execution.
+// In this second iteration, the code is leverages a WaitGroup to provide
+// synchronization between two goroutines; resulting in a non-blocking code
+// where the two function calls run concurrently.
+//
+// IMPORTANT: When working with concurrent code, involved goroutines must NOT
+// share mutable state, as it may cause inconsistencies / bugs. Also, output
+// won't be deterministic (meaning the output may be out of order sometimes)
+// as each function may run out of order achording to the underlying OS scheduler.
 //
 // Sample Output:
 //  Waiting for goroutines to finish...
-//  Printing number 1
-//  Printing number 2
-//  Printing number 3
 //  Generating number 1
 //  Generating number 2
+//  Printing number 1
 //  Generating number 3
+//  Printing number 2
+//  Printing number 3
+//  Done!
 //
 // Source: https://www.digitalocean.com/community/tutorials/how-to-run-multiple-functions-concurrently-in-go
 package main
 
 import (
 	"fmt"
+	"sync"
 )
 
-func generateNumbers(total int) {
+func generateNumbers(total int, wg *sync.WaitGroup) {
+	// Decrease WaitGroup count by one after function ends.
+	defer wg.Done()
 	for idx := 1; idx <= total; idx++ {
 		fmt.Printf("Generating number %d\n", idx)
 	}
 }
 
-func printNumbers() {
+func printNumbers(wg *sync.WaitGroup) {
+	// Decrease WaitGroup count by one after function ends.
+	defer wg.Done()
 	for idx := 1; idx <= 3; idx++ {
 		fmt.Printf("Printing number %d\n", idx)
 	}
 }
 
 func main() {
-	printNumbers()
-	generateNumbers(3)
+	wg := &sync.WaitGroup{}
+	// We expect Done to be called twice as we are calling two functions.
+	wg.Add(2)
+
+	// Creating a goroutine is as simple as prepending the calling function with
+	// the `go` keyword.
+	go printNumbers(wg)
+	go generateNumbers(3, wg)
+
+	fmt.Println("Waiting for goroutines to finish...")
+	// If we remove the Wait() call, code may not fully execute at all, as the
+	// main goroutine may finish before the other subroutines end theirs.
+	wg.Wait()
+	fmt.Println("Done!")
 }
